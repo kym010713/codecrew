81ae684c7a2cc2556fe0f69c048a8e362d448823
kimyoungmin <dudals5608@gmail.com>
2025-05-16 16:52:59 +0900

    init

---
+++ codecrew/src/main/java/com/project/codecrew/CodecrewApplication.java
@@ -0,0 +1,13 @@
+package com.project.codecrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecrewApplication.class, args);
+	}
+
+}
---
+++ codecrew/src/test/java/com/project/codecrew/CodecrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.codecrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
